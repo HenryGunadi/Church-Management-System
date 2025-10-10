5d079bc7109770eab5f260de52aca70a9521f8fb
HenryGunadi <henrywilliamgunadi09@gmail.com>
2025-10-10 18:54:48 +0700

    setup database done, database migration has been added

---
+++ server/src/db/migrations/20251010070022_create_users_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable().unique();
+    table.enu('role', ['admin', 'guest']).notNullable();
+
+    table.timestamps(true, true); // created_at, updated_at
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.schema.dropTable('users');
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world');
+const Server = require('./api/server');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+const server = new Server(process.env.PORT, 'localhost');
+server.run();
