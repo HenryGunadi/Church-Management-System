1617b077c0e4b08715ec414d7cebd1252c0397f7
HenryGunadi <henrywilliamgunadi09@gmail.com>
2025-10-13 16:21:06 +0700

    database health function

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,14 @@
 const Server = require('./api/server');
 const dotenv = require('dotenv');
 const path = require('path');
+const { checkDb } = require('./db/db');
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-const server = new Server(process.env.PORT, 'localhost');
-server.run();
+async function init() {
+    await checkDb();
+    const server = new Server(process.env.PORT, 'localhost');
+    server.run();
+}
+
+init();
